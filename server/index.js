39f4d49d2f6b9058dcc6fcdf03f11f5ee9ba582a
Vaibhavihole31 <itspinki05@gmail.com>
2022-10-30 15:20:57 +0530

    feat: book adding route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
-const path = require('path'); 
+const path = require('path');
 const mongoose = require('mongoose')
+const Book = require("./models/Book")
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,18 +14,38 @@ mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTo
   console.log('Connected to DB 📦');
 });
 
-app.post('/health',(req,res) => {
+app.post('/health', (req, res) => {
   res.json({
     status: "ok",
     message: "Server is running"
   })
 })
 
-if(process.env.NODE_ENV === 'production') {
+app.post('/book', async (req, res) => {
+  const { name, author, description, price, available, image } = req.body
+
+  const newBook = new Book({
+    name,
+    author,
+    description,
+    price,
+    available,
+    image
+  })
+
+  const savedBook = await newBook.save();
+  res.json({
+    success: true,
+    data: savedBook,
+    message: "New Book is added Successfully!"
+  })
+})
+
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
   app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
   });
 }
 
