705234c7b7cd8df8e10675fb0bbc977276994664
Vaibhavihole31 <itspinki05@gmail.com>
2022-10-31 15:21:53 +0530

    feat: added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,43 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home/Home';
+import AddBook from './components/AddBook/AddBook';
+import AboutUs from './components/AboutUs/AboutUs';
 
 function App() {
   return (
-    <div className="container">
-      {/* Remove this code and place your own code here. */}
-      <div class="d-flex align-items-center justify-content-center" style={{height: '80vh'}}>
-        <h1>MERN Starter is Up and Running 🚀</h1>
-      </div>
-    </div>
+    <>
+      <BrowserRouter>
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">BookStore📚</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link active" aria-current="page" to="/">Books</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/addbook">AddBook</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">About Us</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/addbook" element={<AddBook/>} />
+          <Route path="/about" element={<AboutUs/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
