4a59863fb2828a6cbc36fe6df31a239f50113fe2
Vaibhavihole31 <itspinki05@gmail.com>
2022-11-05 14:02:42 +0530

    fixed: delete btn

--- client/src/components/Book/Book.js
+++ client/src/components/Book/Book.js
@@ -5,7 +5,7 @@ function Book(props) {
   return (
     <div className="book-card">
       <div className="row">
-        <div className="col-md-8">
+        <div className="col-md-12">
           <img className='book-img' src={props.image}/>
           <h5>Name: {props.name}</h5>
           <h6>Author: {props.author}</h6>
