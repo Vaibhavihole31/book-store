974ef08479b75b027edbe9f5fdc9652fad53b7ee
Vaibhavihole31 <itspinki05@gmail.com>
2022-11-06 16:16:32 +0530

    ref: update route refactor

--- server/controllers/bookupdate.js
+++ server/controllers/bookupdate.js
@@ -1,27 +1,15 @@
 const Book = require("../models/Book");
 
 const bookUpdate = async (req, res) => {
-    const id = req.params.id;
-    const { name, author, description, price, available, image } = req.body
-    let book;
-    try {
-        book = await Book.findByIdAndUpdate(id, {
-            name,
-            author,
-            description,
-            price,
-            available,
-            image
-        });
-        book = await book.save()
-    } catch (err) {
-        console.log(err);
-    }
+    let result = await Book.updateOne(
+        {_id :req.params.id},
+        {
+            $set : req.body
+        }
+    )
+    res.send(result)
 
-    if (!book) {
-        return res.status(404).json({ message: 'Unable To Update By this Id' })
-    }
-    return res.status(200).json({ book });
 }
 
 module.exports = bookUpdate;
+
